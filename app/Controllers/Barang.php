ac718bfd83b7cd86ff34f17dc561b0b506fc2881
Vikar Maulana Arrisyad <vikar.maulana.arrisyad@gmail.com>
2025-01-21 20:10:47 +0700

    feat: feature insert & upload barang

--- app/Controllers/Barang.php
+++ app/Controllers/Barang.php
@@ -41,5 +41,98 @@ class Barang extends BaseController
         return view('barang/formtambah', $data);
     }
 
-    public function simpandata() {}
+    public function simpandata()
+    {
+        $barangKode = $this->request->getVar('barang_kode');
+        $barangNama = $this->request->getVar('barang_nama');
+        $kategoriId = $this->request->getVar('kategori_id');
+        $satuanId = $this->request->getVar('satuan_id');
+        $barangHarga = $this->request->getVar('barang_harga');
+        $barangStok = $this->request->getVar('barang_stok');
+
+        $validation = \Config\Services::validation();
+
+        $valid = $this->validate([
+            'barang_kode' => [
+                'rules' => 'required|is_unique[barang.barang_kode]',
+                'label' => 'Kode Barang',
+                'errors' => [
+                    'required' => '{field} wajib diisi',
+                    'is_unique' => '{field} kode barang sudah ada sebelumnya'
+                ]
+            ],
+            'barang_nama' => [
+                'rules' => 'required',
+                'label' => 'Nama Barang',
+                'errors' => [
+                    'required' => '{field} wajib diisi',
+                ]
+            ],
+            'kategori_id' => [
+                'rules' => 'required',
+                'label' => 'Kategori',
+                'errors' => [
+                    'required' => '{field} wajib diisi',
+                ]
+            ],
+            'satuan_id' => [
+                'rules' => 'required',
+                'label' => 'Satuan',
+                'errors' => [
+                    'required' => '{field} wajib diisi',
+                ]
+            ],
+            'barang_harga' => [
+                'rules' => 'required|numeric',
+                'label' => 'Harga',
+                'errors' => [
+                    'required' => '{field} wajib diisi',
+                    'numeric' => '{field} hanya dalam bentuk angka',
+                ]
+            ],
+            'barang_stok' => [
+                'rules' => 'required|numeric',
+                'label' => 'Stok',
+                'errors' => [
+                    'required' => '{field} wajib diisi',
+                    'numeric' => '{field} hanya dalam bentuk angka',
+                ]
+            ],
+            'gambar' => [
+                'rules' => 'mime_in[gambar,image/png, image/jpg, image/jpeg]|ext_in[gambar,png,jpg,jpeg]',
+                'label' => 'Gambar',
+            ],
+        ]);
+
+        if (!$valid) {
+            session()->setFlashdata('errors', $this->validator->getErrors());
+            return redirect()->to('/barang/tambah')->withInput();
+        } else {
+            $gambar = $_FILES['gambar']['name'];
+
+            if ($gambar != NULL) {
+                $namaFileGambar = $barangKode;
+                $fileGambar = $this->request->getFile('gambar');
+                $fileGambar->move('upload', $namaFileGambar . '.' . $fileGambar->getExtension());
+
+                $pathGambar = 'upload/' . $fileGambar->getName();
+            } else {
+                $pathGambar = '';
+            }
+
+            // simpan ke dalam database
+            $this->barang->insert([
+                'barang_kode' => $barangKode,
+                'barang_nama' => $barangNama,
+                'kategori_id' => $kategoriId,
+                'satuan_id' => $satuanId,
+                'barang_harga' => $barangHarga,
+                'barang_stok' => $barangStok,
+                'gambar' => $pathGambar,
+            ]);
+
+            session()->setFlashdata('success', 'Data barang berhasil disimpan');
+            return redirect()->to('/barang/tambah');
+        }
+    }
 }
--- app/Views/barang/formtambah.php
+++ app/Views/barang/formtambah.php
@@ -17,59 +17,73 @@ Form Tambah Barang
 <?= $this->endSection() ?>
 
 <?= $this->section('content') ?>
+<?php if (session()->getFlashdata('errors')) : ?>
+    <div class="alert alert-danger">
+        <ul>
+            <?php foreach (session()->getFlashdata('errors') as $error) : ?>
+                <li><?= esc($error) ?></li>
+            <?php endforeach ?>
+        </ul>
+    </div>
+<?php endif; ?>
+<?php if (session()->getFlashdata('success')): ?>
+    <div class="alert alert-success alert-dismissible">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+        <h5><i class="icon fas fa-check"></i> Sukses!</h5>
+        <?= session()->getFlashdata('success'); ?>
+    </div>
+<?php endif; ?>
 <?= form_open_multipart('barang/simpandata') ?>
 <div class="form-group row">
-    <label for="" class="col-sm-3 col-form-label">Kode Barang</label>
+    <label for="barang_kode" class="col-sm-3 col-form-label">Kode Barang</label>
     <div class="col-sm-7">
-        <input type="text" name="barang_kode" id="barang_id" class="form-control" autofocus>
+        <input type="text" name="barang_kode" id="barang_kode" class="form-control <?= session('errors.barang_kode') ? 'is-invalid' : '' ?>" value="<?= old('barang_kode') ?>" autofocus>
     </div>
 </div>
 <div class="form-group row">
-    <label for="" class="col-sm-3 col-form-label">Nama Barang</label>
+    <label for="barang_nama" class="col-sm-3 col-form-label">Nama Barang</label>
     <div class="col-sm-7">
-        <input type="text" name="barang_nama" id="barang_id" class="form-control">
+        <input type="text" name="barang_nama" id="barang_nama" class="form-control <?= session('errors.barang_nama') ? 'is-invalid' : '' ?>" value="<?= old('barang_nama') ?>">
     </div>
 </div>
 <div class="form-group row">
-    <label for="" class="col-sm-3 col-form-label">Pilih Kategori</label>
+    <label for="kategori_id" class="col-sm-3 col-form-label">Pilih Kategori</label>
     <div class="col-sm-7">
-        <select name="kategori_id" id="kategori_id" class="form-control">
+        <select name="kategori_id" id="kategori_id" class="form-control <?= session('errors.kategori_id') ? 'is-invalid' : '' ?>">
             <option selected disabled>Pilih Kategori</option>
-
             <?php foreach ($datakategori as $kat) : ?>
-                <option value="<?= $kat['id'] ?>"><?= $kat['kategori_nama'] ?></option>
+                <option value="<?= $kat['id'] ?>" <?= old('kategori_id') == $kat['id'] ? 'selected' : '' ?>><?= $kat['kategori_nama'] ?></option>
             <?php endforeach; ?>
         </select>
     </div>
 </div>
 <div class="form-group row">
-    <label for="" class="col-sm-3 col-form-label">Pilih Satuan</label>
+    <label for="satuan_id" class="col-sm-3 col-form-label">Pilih Satuan</label>
     <div class="col-sm-7">
-        <select name="satuan_id" id="satuan_id" class="form-control">
+        <select name="satuan_id" id="satuan_id" class="form-control <?= session('errors.satuan_id') ? 'is-invalid' : '' ?>">
             <option selected disabled>Pilih Satuan</option>
-
             <?php foreach ($datasatuan as $sat) : ?>
-                <option value="<?= $sat['id'] ?>"><?= $sat['satuan_nama'] ?></option>
+                <option value="<?= $sat['id'] ?>" <?= old('satuan_id') == $sat['id'] ? 'selected' : '' ?>><?= $sat['satuan_nama'] ?></option>
             <?php endforeach; ?>
         </select>
     </div>
 </div>
 <div class="form-group row">
-    <label for="" class="col-sm-3 col-form-label">Harga</label>
+    <label for="barang_harga" class="col-sm-3 col-form-label">Harga</label>
     <div class="col-sm-7">
-        <input type="number" name="barang_harga" id="barang_harga" class="form-control" min="0">
+        <input type="number" name="barang_harga" id="barang_harga" class="form-control <?= session('errors.barang_harga') ? 'is-invalid' : '' ?>" value="<?= old('barang_harga') ?>" min="0">
     </div>
 </div>
 <div class="form-group row">
-    <label for="" class="col-sm-3 col-form-label">Stok</label>
+    <label for="barang_stok" class="col-sm-3 col-form-label">Stok</label>
     <div class="col-sm-7">
-        <input type="number" name="barang_stok" id="barang_stok" class="form-control" min="0">
+        <input type="number" name="barang_stok" id="barang_stok" class="form-control <?= session('errors.barang_stok') ? 'is-invalid' : '' ?>" value="<?= old('barang_stok') ?>" min="0">
     </div>
 </div>
 <div class="form-group row">
-    <label for="" class="col-sm-3 col-form-label">Upload Gambar <span class="text-danger text-sm">(<i>Jika Ada</i>)</span></label>
+    <label for="gambar" class="col-sm-3 col-form-label">Upload Gambar <span class="text-danger text-sm">(<i>Jika Ada</i>)</span></label>
     <div class="col-sm-7">
-        <input type="file" name="gambar" id="gambar" class="form-control">
+        <input type="file" name="gambar" id="gambar" class="form-control <?= session('errors.gambar') ? 'is-invalid' : '' ?>">
     </div>
 </div>
 <div class="form-group row">
